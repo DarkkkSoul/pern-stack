af7fcbcb668bf7833ab80aec8d68cd298e3d8337
Mahesh <maheshh.kumar1508@gmail.com>
2026-03-07 14:30:16 +0530

    adding pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,16 @@
-import { SignInButton } from "@clerk/react"
-import { Routes, Route } from 'react-dom'
+import { Routes, Route } from 'react-router'
+import Home from "./pages/Home"
+import Product from "./pages/Product"
+import Create from "./pages/Create"
+import Edit from "./pages/Edit"
+import Profile from "./pages/Profile"
+import Navbar from './components/Navbar'
 function App() {
 
   return (
-    <div className='min-h-screen'>
-      <NavBar />
-      <main>
+    <div className='min-h-screen bg-base-100'>
+      <Navbar />
+      <main className='max-w-5xl mx-auto px-4 py-8'>
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/product/:id' element={<Product />} />
